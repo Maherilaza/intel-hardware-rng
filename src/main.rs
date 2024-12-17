1b6ff7e75fd3ce966394fd2221948b0d65c6fd94
Maherilaza <maherilaza11@gmail.com>
2024-12-17 10:58:33 +0300

    add json output

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,60 @@
+use colored::Colorize;
 use core::arch::x86_64::_rdrand64_step;
-use colored::{ColoredString, Colorize};
-use std::{fs::OpenOptions, io::Write};
+use log::{error, info, warn};
+use serde::Serialize;
+use std::{fs::File, io::Write};
 
-const PASS: i8 = 1;
-const ITERATIONS: usize = 10;
+#[derive(Serialize)]
+struct RandomNumbers {
+    numbers: Vec<u64>,
+}
 
 fn main() {
-    let _check_arch = check_arch();
+    env_logger::Builder::new()
+        .filter(None, log::LevelFilter::Info)
+        .init();
 
-    for _ in 0..ITERATIONS {
+    check_arch();
+
+    let mut random_numbers = RandomNumbers { numbers: vec![] };
+
+    for _ in 0..10 {
         let random_number: Result<u64, String> = gen_rand();
 
         if let Ok(nb_rand) = random_number {
-            println!("{} {}", "[*]".yellow(), nb_rand);
-
-            let nb_to_string = format!("{}\n", nb_rand);
-            let mut save_file = match OpenOptions::new()
-                .append(true)
-                .create(true)
-                .open("rand.txt") {
-                    Ok(file) => file,
-                    Err(e) => {
-                        println!("{e}");
-                        return;
-                    }
-                };
-
-            let _write_file = match save_file.write_all(nb_to_string.as_bytes()) {
-                Ok(_) => (),
-                Err(e) => {
-                    println!("{e}");
-                    return;
-                }
-            };
+            // println!("{} {}", "[+]".green(), nb_rand);
+            random_numbers.numbers.push(nb_rand);
         }
+    }
+
+    match save_as_json(&random_numbers, "rand.json") {
+        Ok(_) => info!("Random numbers have been successfully recorded in the file 'rand.json'"),
+        Err(e) => error!("Failed to save JSON file: {}", e),
     }
 }
 
-/// [Intel's documentation]
-/// (<https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#text=_rdrand64_step>)
 fn gen_rand() -> Result<u64, String> {
     let mut random: u64 = 0;
     unsafe {
         let b_check: i32 = _rdrand64_step(&mut random);
-        if b_check != PASS as i32 {
+        if b_check != 1 as i32 {
             return Err(format!("{}", "[-] Cannot generate a random number".red()));
         }
         Ok(random)
     }
 }
 
-fn check_arch() -> Result<(), ColoredString> {
+fn check_arch() {
+    info!("Checking system architecture: x86_64");
     if cfg!(target_arch = "x86") || cfg!(target_arch = "x86_64") {
-        Ok(())
+        info!("System architecture detected: x86_64.");
     } else {
-        Err(format!("{}", "Error: This program requires Intel x86 or x86_64 architecture").red())
+        error!("Error: This program requires Intel x86 or x86_64 architecture");
     }
+}
+
+fn save_as_json(data: &RandomNumbers, filename: &str) -> std::io::Result<()> {
+    let file = File::create(filename)?;
+    serde_json::to_writer_pretty(file, data)?;
+    Ok(())
 }
